1026ff2df0246fe9b2542c12bafde9beb8a8dd20
xriss-xross <xriss_xross@outlook.com>
2025-02-20 21:46:36 +0000

    winit fullscreen window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
-use winit::event_loop::{EventLoop};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowAttributes, WindowId, Fullscreen};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes =
+            WindowAttributes::default()
+                .with_title("Kirian")
+                .with_fullscreen(Some(Fullscreen::Borderless(None)));
+        self.window = Some(event_loop.create_window(
+            window_attributes)
+        .unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("Now exiting...");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-}
 
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+
+    let _ = event_loop.run_app(&mut app);
+}
